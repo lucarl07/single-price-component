51f8b6719e880a75fab8c3fcb934af37143ef973
lucarl07 <junitinho2007@gmail.com>
2024-05-10 16:25:12 -0300

    Arrange the grid items

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,23 @@
-import { Container } from "../Styles/Container.js"
+import { GridArea, Container } from "../Styles/Elements.js"
+import './App.css'
 
 const App = () => {
   return (
     <Container>
-      <div className="section-wrapper"></div>
+      <GridArea className="item01">
+        <h1>Join our community</h1>
+        <h2>30-day, hassle free money back guarantee</h2>
+        <p>
+          Gain access to our full library of tutorials along with expert code reviews.
+          Perfect for any developers who are serious about honing their skills.
+        </p>
+      </GridArea>
+      <GridArea className="item02">
+        <h1>Sou a segunda seção!</h1>
+      </GridArea>
+      <GridArea className="item03">
+        <h1>Sou a terceira seção!</h1>
+      </GridArea>
     </Container>
   );
 }
--- src/components/Styles/Elements.js
+++ src/components/Styles/Elements.js
@@ -2,9 +2,16 @@ import styled from "styled-components";
 import Colors from "./Colors";
 
 export const Container = styled.main `
+  display: grid;
+  grid-template-columns: 300px 300px;
+  grid-template-rows: 240px 240px;
   margin: 0 auto;
   width: 600px;
   height: 480px;
   background-color: ${Colors.white};
   border-radius: 0.75rem;
+`;
+
+export const GridArea = styled.section `
+  
 `;
