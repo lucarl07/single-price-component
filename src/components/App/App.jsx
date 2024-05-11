9d4c05e7cb81efe746a51119e91b9a045d4c7adb
lucarl07 <junitinho2007@gmail.com>
2024-05-10 22:17:25 -0300

    Finish the 3rd grid item

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -25,6 +25,15 @@ const App = () => {
       </GridArea>
       <GridArea className="itemWhyUs">
         <h1>Why Us</h1>
+        <ul className="list">
+          <li>Tutorials by industry experts</li>
+          <li>Peer & expert code review</li>
+          <li>Coding exercises</li>
+          <li>Access to our GitHub repos</li>
+          <li>Community forum</li>
+          <li>Flashcard decks</li>
+          <li>New videos every week</li>
+        </ul>
       </GridArea>
     </Container>
   );
